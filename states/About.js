7498fae656e9aa4e7c1089d9244f70f3e7d96715
Carsten Sandtner <mail@casa-rock.de>
2016-04-10 14:43:15 +0200

    Implemented About State.

--- states/About.js
+++ states/About.js
@@ -6,8 +6,25 @@ Lowrez.About.prototype = {
 		var fontSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,;:?!-_~#\"'&()[]|`\\/@°+=*$£€<>%áéíóú";
 
 		this.game.stage.backgroundColor = '#000000';
-		
+
 		this.backKey = this.input.keyboard.addKey(Phaser.Keyboard.A);
+
+		this.about = this.add.retroFont('font', 6, 11, fontSet, 8, 3, 0);
+		this.about.multiLine = true;
+		this.about.text = "Made by\n" +
+						 "Carsten\n" +
+						 "Sandtner\n\n" +
+						 "@casarock\n\n" +
+						 "Back: A";
+
+		this.logo = this.add.sprite(0, 64, 'logo');
+
+		var tween = this.add.tween(this.logo)
+							.to({x: '-' + this.logo.width/2}, 4000, Phaser.Easing.Quadratic.InOut, true, 0, -1);
+
+		tween.yoyo(true, 0);
+		this.aboutTextImage = this.add.image(4, 66 + this.logo.height, this.about);
+
 	},
 
 	goto: function(state) {
@@ -18,6 +35,13 @@ Lowrez.About.prototype = {
 		if (this.backKey.isDown) {
 			this.goto('MainMenu');
 		}
+
+		if (this.aboutTextImage.position.y < (this.aboutTextImage.height * -1)) {
+			this.aboutTextImage.position.y = 66 + this.logo.height;
+			this.logo.y = 64;
+		}
+		this.aboutTextImage.position.y -= 0.25;
+		this.logo.position.y -= 0.25;
 	},
 
 	render: function() {
